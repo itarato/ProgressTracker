78d2cab8bc2a6776780f134528831ce13893ada0
Peter Arato <it.arato@gmail.com>
2013-08-26 14:38:47 +0100

    Add tests.

---
+++ test/ProgressTrackerTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @file
+ * Example usage
+ */
+
+require_once __DIR__ . '/../ProgressTracker.php';
+
+use ProgressTracker\Tracker as Tracker;
+use ProgressTracker\Reporter as Reporter;
+
+class ProgressTrackerTest extends PHPUnit_Framework_TestCase {
+
+  public $defaultStringReporter;
+
+  public function setUp() {
+    parent::setUp();
+    $this->defaultStringReporter = new Reporter\StringReporter();
+  }
+
+  public function testMemoryTracker() {
+    $mt = new Tracker\ProgressMemoryTracker($this->defaultStringReporter);
+    $this->assertNotEmpty($mt->report());
+  }
+
+  public function testGeneralTracker() {
+    $generalProgress = new Tracker\ProgressGeneralTracker($this->defaultStringReporter);
+
+    for ($i = 10; $i--;) {
+      // usleep(rand(100000, 900000));
+      $this->assertNotEmpty($generalProgress->report());
+    }
+  }
+
+  public function testBatchTracker() {
+    $batchProcess = new Tracker\ProgressBatchTracker($this->defaultStringReporter, 10);
+
+    for ($i = 10; $i--;) {
+      // usleep(500000);
+      $this->assertNotEmpty($batchProcess->report());
+    }
+  }
+
+  public function testArrayReporter() {
+    $r = new Reporter\ArrayReporter();
+    $p = new Tracker\ProgressGeneralTracker($r);
+    $this->assertTrue(is_array($p->report()));
+  }
+
+  public function testStringReporter() {
+    $r = new Reporter\StringReporter();
+    $p = new Tracker\ProgressGeneralTracker($r);
+    $this->assertTrue(is_string($p->report()));
+  }
+
+  public function testSocketIOReporter() {
+    $this->markTestSkipped();
+
+    $r = new Reporter\SocketIOReporter();
+    $generalProgress = new Tracker\ProgressGeneralTracker($r);
+
+    for ($i = 100; $i--;) {
+      usleep(rand(100000, 200000));
+      $generalProgress->report();
+    }
+  }
+
+}
